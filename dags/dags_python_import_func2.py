08720596486e888bff486ddc98acd49447ea1c1d
CodingToastBread <mickeymouset@naver.com>
2025-06-02 21:41:19 +0900

    test

--- dags/dags_python_import_func2.py
+++ dags/dags_python_import_func2.py
@@ -5,7 +5,7 @@ from airflow.operators.python import PythonOperator
 from airflow import DAG
 
 with DAG(
-    dag_id="dags_python_import_func",
+    dag_id="dags_python_import_func2",
     schedule="10 0 * * 6#1",
     start_date=pendulum.datetime(2025, 6, 1, tz="Asia/Seoul"),
     catchup=False,
