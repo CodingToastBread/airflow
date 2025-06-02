5c2fe5e0d79cd7db39a66b32b618492c10c7c2e4
CodingToastBread <mickeymouset@naver.com>
2025-06-02 21:16:34 +0900

    fix

--- dags/dags_python_import_func.py
+++ dags/dags_python_import_func.py
@@ -1,20 +1,14 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
 import pendulum
-
 from common.common_func import get_sftp
 
+from airflow.operators.python import PythonOperator
+from airflow import DAG
 
 with DAG(
     dag_id="dags_python_import_func",
     schedule="30 6 * * *",
-    start_date=pendulum.datetime(2025, 6, 10, tz="Asia/Seoul"),
-    catchup=False
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False,
 ) as dag:
-    
-    task_get_sftp = PythonOperator(
-        task_id="task_get_sftp",
-        python_callable=get_sftp
-    )
-    
-    task_get_sftp
+
+    task_get_sftp = PythonOperator(task_id="task_get_sftp", python_callable=get_sftp)
