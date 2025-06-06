eb86bcee8a3d56a1b3060dfaeb9e5fd85efed85e
CodingToastBread <mickeymouset@naver.com>
2025-06-06 21:35:28 +0900

    macros usage upload2

--- dags/dags_bash_with_macro_eg2.py
+++ dags/dags_bash_with_macro_eg2.py
@@ -5,7 +5,7 @@ import pendulum
 
 with DAG(
     dag_id="dags_bash_with_macro_eg2",
-    schedule="10 0 L * 6#2",
+    schedule="10 0 * * 6#2",
     start_date=pendulum.datetime(2025, 5, 1, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
