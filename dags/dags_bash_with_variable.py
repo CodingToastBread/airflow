c14215b02f7b1aa2a553dbeef51d15f181617372
CodingToastBread <mickeymouset@naver.com>
2025-06-10 23:10:45 +0900

    use variable - upload

---
+++ dags/dags_bash_with_variable.py
@@ -0,0 +1,26 @@
+from tkinter import Variable
+from airflow import DAG
+from airflow.decorators import task
+from airflow.operators.bash import BashOperator
+import pendulum
+
+
+with DAG(
+    dag_id="dags_bash_with_variable",
+    schedule="30 9 * * *",
+    start_date=pendulum.datetime(2025, 6, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    # not recommended
+    var_value = Variable.get("sample_key")
+    
+    bash_var_1 = BashOperator(
+        task_id="bash_var_1",
+        bash_command=f"echo variable:{var_value}",
+    )
+
+    # recommended!
+    bash_var_2 = BashOperator(
+        task_id="bash_var_2",
+        bash_command="echo variable: {{ var.value.sample_key }}"
+    )
