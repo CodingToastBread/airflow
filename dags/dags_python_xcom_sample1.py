308b5314afe4776e61cf1c5ea97b93440c691599
CodingToastBread <mickeymouset@naver.com>
2025-06-14 16:49:42 +0900

    blog-posting xcom-python sample1 upload2

--- dags/dags_python_xcom_sample1.py
+++ dags/dags_python_xcom_sample1.py
@@ -16,27 +16,42 @@ with DAG(
         ti = kwargs["ti"]
 
         # xcom_push 메소드로 공유 데이터 INSERT
-        ti.xcom_push(key="key1", value="value1")
-        ti.xcom_push(key="list1", value=[1, 2])
+        ti.xcom_push(key="key", value="value11111")
+        ti.xcom_push(key="list", value=[1, 2, 3])
         ti.xcom_push(key="map1", value={"name": "coding_toast"})
-        return "pushing_task 111111 return value"
+        return "return 111111"
 
-    # @task(task_id="pushing_task_2")
-    # def pushing_task_2(**kwargs):
-    #     ti = kwargs["ti"]
-    #     ti.xcom_push(key="key2", value="value2")
-    #     ti.xcom_push(key="list2", value=[3, 4])
-    #     return "pushing_task 222222 return value"
+    @task(task_id="pushing_task_2")
+    def pushing_task_2(**kwargs):
+        ti = kwargs["ti"]
+        ti.xcom_push(key="key", value="value22222")
+        ti.xcom_push(key="list", value=[4, 5, 6])
+        return "return 222222"
 
     @task(task_id="pulling_task")
     def pulling_task(**kwargs):
+
+        # push task 의 구동 순서 때문에 pushing_task_2 의 세팅값이 조회됩니다.
         ti = kwargs["ti"]
-        keyVal = ti.xcom_pull(key="key1")
-        listVal = ti.xcom_pull(key="list1")
-        return_value = ti.xcom_pull(task_ids="pushing_task_1")
+        keyVal = ti.xcom_pull(key="key")
+        listVal = ti.xcom_pull(key="list")
+        return_value = ti.xcom_pull(key='return_value')
+
+        print("task_ids 값 주지 않고 조회하기:")
         print(keyVal)
         print(listVal)
         print(return_value)
 
-    # xcom_push1() >> xcom_push2() >> xcom_pull()
-    pushing_task_1() >> pulling_task()
+        # task_id 인자값을 줘야만 같은 key 명칭을 쓰더라도 정확히 구분해서 조회할 수 있습니다.
+        ti = kwargs["ti"]
+        keyVal = ti.xcom_pull(key="key", task_ids="pushing_task_1")
+        listVal = ti.xcom_pull(key="list", task_ids="pushing_task_1")
+        return_value = ti.xcom_pull(key='return_value' # key='return_value' 는 생략가능!
+                                    , task_ids="pushing_task_1") 
+        
+        print('task_ids="pushing_task_1" 세팅 후 조회하기:')
+        print(keyVal)
+        print(listVal)
+        print(return_value)
+
+    pushing_task_1() >> pushing_task_2() >> pulling_task()
