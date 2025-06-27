760d8dfe5a45dde2c77b9dc989681ca2a63db161
sampark1234 <sampark1234@wavus.co.kr>
2025-06-27 14:55:45 +0900

    dags_python_with_custom_hook_bulk_load222 upload7

---
+++ dags/dags_python_with_custom_hook_bulk_load_heavy.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+import pendulum
+
+from hooks.custom_postgres_hook_advance import CustomPostgresAdvanceHook
+
+
+with DAG(
+    dag_id='dags_python_with_custom_hook_bulk_load_222',
+    start_date=pendulum.datetime(2025, 6, 1, tz='Asia/Seoul'),
+    schedule=None,
+    catchup=False
+) as dag:
+
+    def insrt_postgres(postgres_conn_id, tbl_nm, file_nm, **kwargs):
+        custom_postgres_hook = CustomPostgresAdvanceHook(postgres_conn_id=postgres_conn_id)
+        custom_postgres_hook.bulk_load(table_name=tbl_nm, file_name=file_nm, delimiter=',', is_header=True, is_replace=True)
+
+    insrt_postgres = PythonOperator(
+        task_id="insrt_postgres",
+        python_callable=insrt_postgres,
+        op_kwargs={
+            "postgres_conn_id": "conn-db-postgres-custom",
+            "tbl_nm": "customers_info",
+            'file_nm': "/opt/airflow/files/csv_sample/AL_D161_11_20250607.csv",
+        },
+    )
