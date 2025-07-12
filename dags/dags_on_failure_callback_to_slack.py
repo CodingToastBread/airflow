e03c9baf021a07516640cdab163434e7998b065e
CodingToastBread <mickeymouset@naver.com>
2025-07-12 14:32:46 +0900

    slack practice3

--- dags/dags_on_failure_callback_to_slack.py
+++ dags/dags_on_failure_callback_to_slack.py
@@ -7,25 +7,25 @@ from config.on_failure_callback_to_slack import on_failure_callback_to_slack
 
 
 with DAG(
-    dag_id="dags_on_failure_callback_to_slack",
-    start_date=pendulum.datetime(2025,7,1,tz="Asia/Seoul"),
-    schedule="*/20 * * * *",
+    dag_id='dags_on_failure_callback_to_slack',
+    start_date=pendulum.datetime(2025, 7, 1, tz='Asia/Seoul'),
+    schedule='0 * * * *',
     catchup=False,
     default_args={
-        'on_failure_callback': on_failure_callback_to_slack,
+        'on_failure_callback':on_failure_callback_to_slack,
         'execution_timeout': timedelta(seconds=60)
     }
+
 ) as dag:
     task_slp_90 = BashOperator(
         task_id='task_slp_90',
-        bash_command='sleep 90'
+        bash_command='sleep 90',
     )
-    
+
     task_ext_1 = BashOperator(
         trigger_rule='all_done',
         task_id='task_ext_1',
         bash_command='exit 1'
     )
 
-    # 2개의 task 모두 실패할 예정!
     task_slp_90 >> task_ext_1
